2746128ecfe7e2d9a6afa574a6641daabbcd659a
Vedang <vedang.wappnet@gmail.com>
2025-02-23 13:04:40 +0530

    server file is created

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { connectDB } = require("./config/dbConfig");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// connect to the database
+connectDB();
+
+app.use("/categories", require("./routes/categoryRoutes"));
+app.use("/subcategories", require("./routes/subCategoryRoutes"));
+// app.use("/products", require("./routes/productRoutes"));
+// app.use("/category-discounts", require("./routes/categoryDiscountRoutes"));
+
+app.listen(process.env.PORT, () => console.log(`Product Service running on port ${process.env.PORT}`));
