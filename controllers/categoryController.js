7705f7d5917a2b2559f1ff3b402cd92e9643d428
Vedang <vedang.wappnet@gmail.com>
2025-02-23 12:42:45 +0530

    category controller is created

---
+++ controllers/categoryController.js
@@ -0,0 +1,34 @@
+const Category = require("../models/category");
+
+exports.createCategory = async (req, res) => {
+    try{
+        const { name } = req.body;
+        const category = await Category.create({ name });
+        res.status(201).json({
+            success: true,
+            message: "New category is created successfully"
+        });
+    }catch(error){
+        console.log("Error in creating a new Category: ", error.message);
+        return res.status(500).json({
+            success: false,
+            message: "Failure in creting a category"
+        });
+    }
+};
+
+exports.getCategories = async (req, res) => {
+    try{
+        const categories = await Category.findAll();
+        return res.status(200).json({
+            success: true,
+            categories
+        });
+    }catch(error){
+        console.error("Error in fetching all categories: ". error.message);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
