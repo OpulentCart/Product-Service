d01d0c07abcd110362ea1bd6e4a84e85fd7fdfbb
Vedang <vedang.wappnet@gmail.com>
2025-02-23 14:57:56 +0530

    update & delete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -32,3 +32,37 @@ exports.getCategories = async (req, res) => {
         });
     }
 };
+
+exports.updateCategory = async(req, res) => {
+    try{
+        const { category_id, name } = req.body;
+        await Category.update({ name: name}, {where: {category_id: category_id}}); 
+        return res.status(201).json({
+            success: true,
+            message: "Updates are applied"
+        });
+    }catch(error){
+        console.error("Error in updaing category", error.message);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
+
+exports.deleteCategory = async(req, res) => {
+    try{
+        const { category_id } = req.body;
+        await Category.destroy({ category_id: category_id});
+        return res.status(201).json({
+            success: true,
+            message: "Category deleted successfully"
+        });
+    }catch(error){
+        console.error("Error in deleting Category", error.message);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+};
