d47746b48e68eebdf7e404bdde76aaf8b5f27ae6
Vedang <vedang.wappnet@gmail.com>
2025-02-23 15:00:40 +0530

    routes added

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,5 +4,7 @@ const categoryController = require("../controllers/categoryController");
 
 router.post("/create", categoryController.createCategory);
 router.get("/", categoryController.getCategories);
+router.put("/", categoryController.updateCategory);
+router.delete("/", categoryController.deleteCategory);
 
 module.exports = router;
--- routes/sub_categoryRoutes.js
+++ routes/sub_categoryRoutes.js
@@ -4,5 +4,7 @@ const subCategoryController = require("../controllers/sub_categoryController");
 
 router.post("/create", subCategoryController.createSubCategory);
 router.get("/", subCategoryController.getSubCategories);
+router.put("/", subCategoryController.updateSubCategory);
+router.delete("/", subCategoryController.deleteSubCategory);
 
 module.exports = router;
