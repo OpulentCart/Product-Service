b2e79dd1212fecd4542f89d76ff803192be949f8
Vedang <vedang.wappnet@gmail.com>
2025-02-25 12:05:23 +0530

    added route

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controllers/categoryController");
+const { authenticateUser, authorizeRole } = require('../middleware/authMiddleware');
 
 router.post("/create", categoryController.createCategory);
 router.get("/", categoryController.getCategories);
 router.put("/:id", categoryController.updateCategory);
 router.delete("/:id", categoryController.deleteCategory);
+router.get("/count/category", authenticateUser, authorizeRole('admin'), categoryController.getTotalCategoryCount);
 
 module.exports = router;
