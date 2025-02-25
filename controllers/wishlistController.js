33f48a5d929a8a50920b2968e0da0052bf77f093
Vedang <vedang.wappnet@gmail.com>
2025-02-26 00:23:27 +0530

    add to wishlist implemented

--- controllers/wishlistController.js
+++ controllers/wishlistController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 const Wishlist = require('../models/wishlist');
 
+// get wishlist of the customer
 exports.getWishlist = async(req, res) => {
     try{
         const user_id = req.user.user_id;
@@ -16,13 +17,38 @@ exports.getWishlist = async(req, res) => {
         });
         return res.status(200).json({
             success: true,
-            wishlist
+            data: wishlist
         });
     }catch(error){
         console.error("Error in getting wishList: ", error.message);
         return res.status(500).json({
             success: false,
             message: "Failed to get your wishlist"
+        });
+    }
+};
+
+// add product to the wish-list
+exports.addToWishlist = async(req, res) => {
+    try{
+        const user_id = req.user.user_id;
+        const { product_id } = req.body;
+        // Check if product already exists in the wishlist
+        const existingItem = await Wishlist.findOne({ where: { user_id, product_id } });
+        if (existingItem) {
+            return res.status(400).json({ success: false, message: "Product already in wishlist" });
+        }
+        
+        // Add to wishlist
+        const wishlistItem = await Wishlist.create({ user_id, product_id });
+
+        return res.status(201).json({ success: true, data: wishlistItem });
+    
+    }catch(error){
+        console.error('Error in adding product to wish-list: ', error.message);
+        return res.status(500).json({
+            success: false,
+            message: 'Failed to add-product in to the wishlist'
         });
     }
 };
