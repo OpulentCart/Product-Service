f6ed6582b1a44ae5cf6e52b0fab9cf287e07c0a6
Vedang <vedang.wappnet@gmail.com>
2025-02-25 11:57:11 +0530

    changes done

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,8 +8,9 @@ router.get('/admin', productController.getAllProducts);
 router.get('/category/:id', productController.getAllProductsByCategory);
 router.get('/sub_category/:id', productController.getAllProductsBySubCategory);
 router.put('/:id', productController.updateProductStatus);
-router.get('/:id', productController.getAllProductsOfVendor);
+router.get('/vendor/:id', productController.getAllProductsOfVendor);
 router.get('/customer/products', authenticateUser , authorizeRole('customer'), productController.getAllProductsForCustomer);
 router.get('/count/products', authenticateUser, authorizeRole('admin'), productController.getTotalProductsCount);
+router.get('/:id', authenticateUser, productController.getProductById);
 
 module.exports = router;
