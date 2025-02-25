7101ac871000bebfbd07eec4008416526133dfae
Vedang <vedang.wappnet@gmail.com>
2025-02-25 22:52:06 +0530

    wishlist table is created

---
+++ models/wishlist.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../config/dbConfig');
+
+const Wishlist = sequelize.define('Wishlist', {
+    wishlist_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'auth_app_customuser',
+            key: 'id'
+        },
+        onDelete: 'CASCADE'
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'product',
+            key: 'product_id'
+        },
+        onDelete: 'CASCADE'
+    }
+}, {
+    tableName: 'wishlist',
+    timestamps: true
+});
+
+
+// sync model with database
+sequelize.sync({ alter: true})
+    .then(() => {
+        console.log("Wishlist table is created")
+    })
+    .catch(err => console.error("❌ Error creating wishlist table:", err));
+
+module.exports = Wishlist;
